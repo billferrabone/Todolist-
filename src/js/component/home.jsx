d6efa8de083ddfb7024e5a1b8147eed5a6172c7c
billferrabone <billferrabone@gmail.com>
2022-08-25 03:16:58 +0000

    logica lista

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useState } from "react";// 1. importo el hook useState
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [tarea, setTarea] = useState(""); // 2. lo ejecuto	
+	const [lista, setLista] = useState([])
+	function submit(e) {
+		e.preventDefault()
+		console.log(tarea);
+		setTarea("")
+		setLista([...lista, tarea])
+	}
+	// function impresion(e) {
+	// 	setLista(currentFruits => currentFruits.concat('Manzana'))
+	// }	
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5">ToDo List</h1>
+		<form className="container" onSubmit={submit}>
+		<div className="mb-3">
+			<label htmlFor="exampleInputEmail1" className="form-label"></label>
+			<input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="What needs to be done?"
+			onChange={(e)=>setTarea(e.target.value)}
+			// onKeyPress={(e)=>setTarea(e.target.value)}
+			value={tarea}
+			/>
+			<div id="emailHelp" className="form-text"></div>
+		</div>
+		<div className="mb-3">
+			{lista.map((item)=><li>{item}</li>)}
+		</div>
+		</form>
 		</div>
 	);
 };
