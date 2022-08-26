369e639ffb2ec67847b7f0378671b17921c37e86
billferrabone <billferrabone@gmail.com>
2022-08-26 02:15:22 +0000

    estetica fallida

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -12,27 +12,37 @@ const Home = () => {
 		setTarea("")
 		setLista([...lista, tarea])
 	}
-	// function impresion(e) {
-	// 	setLista(currentFruits => currentFruits.concat('Manzana'))
-	// }	
+		
 	return (
+		<div className="list-group" onSubmit={submit}>
 		<div className="text-center">
 			<h1 className="text-center mt-5">ToDo List</h1>
-		<form className="container" onSubmit={submit}>
-		<div className="mb-3">
-			<label htmlFor="exampleInputEmail1" className="form-label"></label>
-			<input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="What needs to be done?"
+		</div>
+		<a href="#" className="list-group-item list-group-item-action"><input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="What needs to be done?"
 			onChange={(e)=>setTarea(e.target.value)}
-			// onKeyPress={(e)=>setTarea(e.target.value)}
 			value={tarea}
 			/>
-			<div id="emailHelp" className="form-text"></div>
+			</a>
+		<a href="#" className="list-group-item list-group-item-action">	{lista.map((item)=><li>{item}</li>)}</a>
 		</div>
-		<div className="mb-3">
-			{lista.map((item)=><li>{item}</li>)}
-		</div>
-		</form>
-		</div>
+
+		// <div className="text-center">
+		// 	<h1 className="text-center mt-5">ToDo List</h1>
+		// <form className="container" onSubmit={submit}>
+		// <div className="mb-3">
+		// 	<label htmlFor="exampleInputEmail1" className="form-label"></label>
+		// 	<input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="What needs to be done?"
+		// 	onChange={(e)=>setTarea(e.target.value)}
+		// 	// onKeyPress={(e)=>setTarea(e.target.value)}
+		// 	value={tarea}
+		// 	/>
+		// 	<div id="emailHelp" className="form-text"></div>
+		// </div>
+		// <div className="mb-3">
+		// 	{lista.map((item)=><li>{item}</li>)}
+		// </div>
+		// </form>
+		// </div>
 	);
 };
 
